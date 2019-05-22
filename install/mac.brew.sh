1641fa9e465897286e45862c34521bb84b9bd6e6
devopsotrator <devopsotrator@imp.to>
2019-05-22 10:47:31 +0300

    brew install edited

--- install/mac.brew.sh
+++ install/mac.brew.sh
@@ -4,12 +4,15 @@ set -e
 
 # generic mac/bash setup
 brew install python3
-brew install coreutils  # GNU version of core utils (grep, cat, ..)
-brew install moreutils  # extras like parallel https://rentes.github.io/unix/utilities/2015/07/27/moreutils-package/
-brew install findutils	# locate, updatedb, find, xargs
+brew install coreutils
+brew install moreutils
+brew install findutils
 brew install shellcheck
-brew install tldr	# https://github.com/tldr-pages/tldr
-brew instal htop
+
+# https://github.com/tldr-pages/tldr
+brew install tldr
+
+# brew instal htop
 
 brew install fzf
 /usr/local/opt/fzf/install
